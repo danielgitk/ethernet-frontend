55266babce69f4255f40383ef561d26c7b77457f
abeni09 <abenij09@gmail.com>
2020-11-25 01:56:32 -0800

    tenderView update

--- src/app/tenders/tenders.component.ts
+++ src/app/tenders/tenders.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+interface sortBy1 {
+  value: string;
+  viewValue: string;
+}
+interface sortBy2 {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-tenders',
   templateUrl: './tenders.component.html',
   styleUrls: ['./tenders.component.css']
 })
 export class TendersComponent implements OnInit {
+
+  deadlineOrTitle: sortBy1[] = [
+    {value: 'Deadline', viewValue: 'Deadline'},
+    {value: 'Title', viewValue: 'Title'}
+  ];
+  AscendOrDescend: sortBy2[] = [
+    {value: 'Ascending', viewValue: 'Ascending'},
+    {value: 'Descending', viewValue: 'Descending'}
+  ];
 
   constructor() { }
 
